200ae2d78a29de1db794fe8b14a0ddd26c53ded1
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-10-21 15:10:22 +0100

    fonts Addition

---
+++ constants/icons.ts
@@ -0,0 +1,18 @@
+import navLogo from "../public/assests/icons/Group (1).svg"
+import footerLogo from "../public/assests/icons/Group.svg"
+import star from "../public/assests/icons/Star 5.svg"
+import twitter from "../public/assests/icons/    (1).svg"
+import google from "../public/assests/icons/    (2).svg"
+import linkdin from "../public/assests/icons/    (4).svg"
+import facebook from "../public/assests/icons/   .svg"
+
+
+export default{
+    navLogo,
+    footerLogo,
+    star,
+    twitter,
+    google,
+    linkdin,
+    facebook
+}
---
+++ constants/images.ts
@@ -0,0 +1,16 @@
+import heroBg from '../public/assests/images/shutterstock_524513014 1.png'
+import profilePic from '../public/assests/images/160-1600378_transparent-happy-person-png-happy-man-face-png 1.png'
+import product1 from '../public/assests/images/Rectangle 7.png'
+import product2 from '../public/assests/images/Vape-Pen 1.png'
+import aboutBg from '../public/assests/images/shutterstock_1304655307 1.png'
+import missionBg from '../public/assests/images/Group 3.png'
+
+
+export default{
+    heroBg,
+    profilePic,
+    product1,
+    product2,
+    aboutBg,
+    missionBg
+}
