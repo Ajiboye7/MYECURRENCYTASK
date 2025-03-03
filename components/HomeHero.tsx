4b9a9c53173e825b9addb56fbcd32d226de9059a
Ajiboye <ajiboyemuyideen7@gmail.com>
2025-03-03 20:48:58 +0100

    Updated codebase

--- components/HomeHero.tsx
+++ components/HomeHero.tsx
@@ -7,13 +7,7 @@ const Hero = () => {
   return (
     <section className="bg-hero-bg bg-cover bg-center h-[90vh] w-full">
       <div className=" flex w-full h-full px-5">
-        <div className="container mx-auto mt-52 sm:mt-72 ">
-          <p className="sm:text-[16px] text-[18px] font-normal text-primary">
-            We are here to help you
-          </p>
-          <p className="text-[40px] sm:text-[60px] font-bold mt-4 text-primary">
-            Relax & Rest
-          </p>
+        <div className="co
           <p className="text-[13px] sm:text-[16px] max-w-[300px] md:max-w-[600px] text-w font-normal leading[20px] text-primary ">
             With the aid of our Melatonin Sleepstiq, we can assure you that you
             can
@@ -43,7 +37,15 @@ const Hero = () => {
             className="w-[90px] sm:w-auto"
           />
           <Image
-            s
+            src={images.bloomberg}
+            alt="bloomberg"
+            width={226}
+            height={44}
+            className="w-[90px] sm:w-auto"
+          />
+          <Image
+            src={images.sleepReview}
+            alt="sleepReview"
             width={201}
             height={48}
             className="w-[90px] sm:w-auto"
