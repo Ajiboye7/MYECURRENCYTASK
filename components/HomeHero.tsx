a5a35d3796cc3d386319efeed29fd96ed5bdbb0e
Ajiboye <ajiboyemuyideen7@gmail.com>
2025-03-09 21:24:37 +0100

    fixed mobile navabar

--- components/HomeHero.tsx
+++ components/HomeHero.tsx
@@ -19,7 +19,18 @@ const Hero = () => {
             can
             <br /> get quality sleep.
           </p>
- 
+
+          <div className="mt-7">
+            <CustomButton title="Visit Shop" link="/shop" />
+          </div>
+        </div>
+      </div>
+
+      <div className="sm:absolute sm:bottom-10 right-0 w-full max-w-[1500px] h-[131px] rounded-[2px] px-5 sm:px-4 bg-sponsorBg shadow-2xl container">
+        <div className="flex flex-wrap items-center justify-around mx-4 h-full gap-4">
+          <Image
+            src={images.google}
+            alt="google"
             width={146}
             height={51}
             className="w-[90px] sm:w-auto"
