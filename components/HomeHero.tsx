120370374662e7606c37fc52e61e98bfd7fa7651
Ajiboye <ajiboyemuyideen7@gmail.com>
2025-03-20 18:46:12 +0100

    updated

--- components/HomeHero.tsx
+++ components/HomeHero.tsx
@@ -13,25 +13,6 @@ const Hero = () => {
           </p>
           <p className="text-[40px] sm:text-[60px] font-bold mt-4 text-primary">
             Relax & Rest
-          </p>
-          <p className="text-[13px] sm:text-[16px] max-w-[300px] md:max-w-[600px] text-w font-normal leading[20px] text-primary ">
-            With the aid of our Melatonin Sleepstiq, we can assure you that you
-            can
-            <br /> get quality sleep.
-          </p>
-
-          <div className="mt-7">
-            <CustomButton title="Visit Shop" link="/shop" />
-          </div>
-        </div>
-      </div>
-
-      <div className="sm:absolute sm:bottom-10 right-0 w-full max-w-[1500px] h-[131px] rounded-[2px] px-5 sm:px-4 bg-sponsorBg shadow-2xl container">
-        <div className="flex flex-wrap items-center justify-around mx-4 h-full gap-4">
-          <Image
-            src={images.google}
-            alt="google"
-            width={146}
             height={51}
             className="w-[90px] sm:w-auto"
           />
