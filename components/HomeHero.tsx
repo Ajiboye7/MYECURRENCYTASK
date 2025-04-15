73159b9014bbef43b3eabec502db1aa12f7e68c5
Ajiboye <ajiboyemuyideen7@gmail.com>
2025-04-15 19:37:19 +0100

    updated

--- components/HomeHero.tsx
+++ components/HomeHero.tsx
@@ -26,6 +26,15 @@ const Hero = () => {
         </div>
       </div>
 
+      <div className="sm:absolute sm:bottom-10 right-0 w-full max-w-[1500px] h-[131px] rounded-[2px] px-5 sm:px-4 bg-sponsorBg shadow-2xl container">
+        <div className="flex flex-wrap items-center justify-around mx-4 h-full gap-4">
+          <Image
+            src={images.google}
+            alt="google"
+            width={146}
+            height={51}
+            className="w-[90px] sm:w-auto"
+          />
           <Image
             src={images.forbes}
             alt="forbes"
