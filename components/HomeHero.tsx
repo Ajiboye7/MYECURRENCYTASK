7812fead49279b89b0c57238415c31e7535678c7
Ajiboye <ajiboyemuyideen7@gmail.com>
2025-05-13 19:30:50 +0100

    updated

--- components/HomeHero.tsx
+++ components/HomeHero.tsx
@@ -8,13 +8,7 @@ const Hero = () => {
     <section className="bg-hero-bg bg-cover bg-center h-[90vh] w-full">
       <div className=" flex w-full h-full px-5">
         <div className="container mx-auto mt-52 sm:mt-72 ">
-          <p className="sm:text-[16px] text-[18px] font-normal text-primary">
-            We are here to help you
-          </p>
-          <p className="text-[40px] sm:text-[60px] font-bold mt-4 text-primary">
-            Relax & Rest
-          </p>
-          <p className="text-[13px] sm:text-[16px] max-w-[300px] md:max-w-[600px] text-w font-normal leading[20px] text-primary ">
+   sm:text-[16px] max-w-[300px] md:max-w-[600px] text-w font-normal leading[20px] text-primary ">
             With the aid of our Melatonin Sleepstiq, we can assure you that you
             can
             <br /> get quality sleep.
